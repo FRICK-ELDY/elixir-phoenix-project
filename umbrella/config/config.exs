1b51ae2f49641c92b9094ce2138d0f66a2d9c914
FRICK-ELDY <ncssano0x0000@gmail.com>
2025-07-12 17:18:33 +0900

    add phx app canvas

--- umbrella/config/config.exs
+++ umbrella/config/config.exs
@@ -7,7 +7,40 @@
 # all use the same configuration file. If you want different
 # configurations or dependencies per app, it is best to
 # move said applications out of the umbrella.
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
 import Config
+
+config :canvas,
+  generators: [timestamp_type: :utc_datetime]
+
+# Configures the endpoint
+config :canvas, CanvasWeb.Endpoint,
+  url: [host: "localhost"],
+  adapter: Bandit.PhoenixAdapter,
+  render_errors: [
+    formats: [html: CanvasWeb.ErrorHTML, json: CanvasWeb.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: Canvas.PubSub,
+  live_view: [signing_salt: "RaKBYRoV"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
 
 # Sample configuration:
 #
